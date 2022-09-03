5d7c4de5b5ea57e9aab0f9dc7e138938d5bdd138
jilongfei <411358465@qq.com>
2022-09-03 19:43:17 +0800

    first commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,8 +2,6 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git222");
-        System.out.println(3333333);
+        System.out.println("hello git111");
     }
 }
